393725215bdaf0243deb037661fb8b39e3026883
kingofbanana <82736124@qq.com>
2017-06-13 20:31:52 +0800

    there are some important information in response.body like coupon count, url and so on, so we use slimerJS to parse it

---
+++ JDtest_slimer.js
@@ -0,0 +1,28 @@
+var url="https://a.jd.com";
+
+var fs=require('fs');
+var page = require('webpage').create();
+
+fs.makeTree('contents');
+
+page.captureContent = [ /.*/ ];
+
+page.onResourceReceived = function(response) {
+console.log('Response (#' + response.id + ', stage "' + response.stage + '"): ' + JSON.stringify(response));
+
+// if(response.stage!="end" || !response.bodySize)return;
+
+// var matches = response.url.match(/[/]([^/]+)$/);
+// var fname = "contents/"+matches[1];
+
+// console.log("Saving "+response.bodySize+" bytes to "+fname);
+// fs.write(fname,response.body);
+};
+
+page.onResourceRequested = function(requestData, networkRequest) {
+//console.log('Request (#' + requestData.id + '): ' + JSON.stringify(requestData));
+};
+
+page.open(url,function(){
+    phantom.exit();
+});
